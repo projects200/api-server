35c4bfadcdbd20ff8a36f2e646922c9bcb9a555e
kim seung gyu <rlatmdrb1996@gmail.com>
2025-04-28 14:45:57 +0900

    chore(project): 스프링 초기 설정(#66)
    스프링 초기 설정
    - 종속성 추가
    - gitignore 추가

---
+++ src/test/java/store/undabang/undabang/UndabangApplicationTests.java
@@ -0,0 +1,13 @@
+package store.undabang.undabang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndabangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
