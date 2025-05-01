efe99b347e80136aacd8de6bc75a827be4a3176e
JooH <nexon9093@gmail.com>
2025-05-01 15:33:58 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd /home/ec2-user/deploy/zip/
 
 docker-compose down
 
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 229262694893.dkr.ecr.ap-northeast-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 825773631552.dkr.ecr.ap-northeast-2.amazonaws.com/undabang-repository
 docker-compose pull
 
 docker-compose up -d
