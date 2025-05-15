fab57f7b805169c327f2d02ea8c2d820be4bf656
JooH <nexon9093@gmail.com>
2025-05-15 15:49:26 +0900

    fix(회원) : Enum 값에 다른 데이터가 들어온 경 예외처리  (#84)

--- src/main/java/com/project200/undabang/member/dto/request/SignUpRequestDto.java
+++ src/main/java/com/project200/undabang/member/dto/request/SignUpRequestDto.java
@@ -1,7 +1,8 @@
 package com.project200.undabang.member.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.project200.undabang.member.deserializer.MemberGenderDeserializer;
 import com.project200.undabang.member.enums.MemberGender;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -16,13 +17,14 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequestDto {
-    @NotNull(message = "성별을 올바르게 입력하세요")
+    @NotNull(message = "성별을 입력해주세요")
+    @JsonDeserialize(using = MemberGenderDeserializer.class)
     private MemberGender memberGender;
 
     @NotNull (message = "생일을 입력하세요")
     private LocalDate memberBday;
 
-    @NotBlank (message = "닉네임을 설정해주세요")
+    @NotNull (message = "닉네임을 설정해주세요")
     @Size(min=1, max = 30, message = "닉네임은 30자 이내로 설정해주세요")
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "닉네임은 한글, 영문, 숫자만 사용 가능합니다")
     private String memberNickname;
