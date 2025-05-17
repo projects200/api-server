753aaa37058386d6a93847b969aa5f0b835e8e1e
JooH <nexon9093@gmail.com>
2025-05-18 00:02:40 +0900

    feat(운동기록): 테스트 코드 작성을 위한 repo 작성 (#40)

---
+++ src/main/java/com/project200/undabang/member/repository/MemberLocationRepository.java
@@ -0,0 +1,7 @@
+package com.project200.undabang.member.repository;
+
+import com.project200.undabang.member.entity.MemberLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberLocationRepository extends JpaRepository<MemberLocation, Long> {
+}
