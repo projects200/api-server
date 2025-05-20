aa0a6f6d9420a058d8878b9802a5e6dad39e00a5
kim seung gyu <rlatmdrb1996@gmail.com>
2025-05-21 05:01:28 +0900

    feat(exercise): 운동 기록 생성 API 개발(#38)
    - LocalStack S3 버킷 자동 생성
    - LocalStack S3 설정 변경
    
    Signed-off-by: kim seung gyu <rlatmdrb1996@gmail.com>

--- init-scripts/init-s3-buckets.sh
+++ init-scripts/init-s3-buckets.sh
@@ -2,7 +2,6 @@
 
 echo "S3 버킷 생성을 시작합니다..."
 
-# 'my-auto-bucket-1' 이름으로 버킷 생성
-awslocal s3 mb s3://my-local-image-bucket
+awslocal s3api create-bucket --bucket my-local-image-bucket
 
 echo "S3 버킷 생성이 완료되었습니다."
