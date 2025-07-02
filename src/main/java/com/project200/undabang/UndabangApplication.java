0ea9318750c676f1beaf3531eba32805ddcc02c5
Seunggyu Kim <rlatmdrb1996@gmail.com>
2025-07-03 00:09:53 +0900

    refactor: 프로파일 변경 및 불필요 코드 정리
    - test의 설정 파일을 `application-{env}-docs.yml` 형식으로 변경
    - 깃허브 액션 파일에 변경사항 반영
    - 사용하지 않는 import 및 코드 제거 (UndabangApplication.java)
    
    Signed-off-by: Seunggyu Kim <rlatmdrb1996@gmail.com>

--- src/main/java/com/project200/undabang/UndabangApplication.java
+++ src/main/java/com/project200/undabang/UndabangApplication.java
@@ -2,14 +2,6 @@ package com.project200.undabang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class UndabangApplication {
