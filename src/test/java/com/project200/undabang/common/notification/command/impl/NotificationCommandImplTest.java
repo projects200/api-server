9c18dd379fbe000c8b03a2bd550f22c67bab5b5d
JooH <nexon9093@gmail.com>
2025-07-24 22:59:26 +0900

    feat(notification) : 테스트 패키지 설정 (#158)

---
+++ src/test/java/com/project200/undabang/common/notification/command/impl/NotificationCommandImplTest.java
@@ -0,0 +1,12 @@
+package com.project200.undabang.common.notification.command.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotificationCommandImplTest {
+
+    @Test
+    void sendErrorNotification() {
+    }
+}
---
+++ src/test/java/com/project200/undabang/common/notification/notifier/impl/SlackNotifierAdapterTest.java
@@ -0,0 +1,12 @@
+package com.project200.undabang.common.notification.notifier.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SlackNotifierAdapterTest {
+
+    @Test
+    void testNotify() {
+    }
+}
