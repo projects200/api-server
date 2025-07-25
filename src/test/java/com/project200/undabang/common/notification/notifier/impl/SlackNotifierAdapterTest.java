3b93a017b4131f3f5a3689506cdcf403dd6d7383
JooH <nexon9093@gmail.com>
2025-07-25 22:53:38 +0900

    feat(notification) : WireMock을 사용하여 SlackNotifierAdapter 테스트코드 추가 (#158)

--- src/test/java/com/project200/undabang/common/notification/notifier/impl/SlackNotifierAdapterTest.java
+++ src/test/java/com/project200/undabang/common/notification/notifier/impl/SlackNotifierAdapterTest.java
@@ -1,12 +1,107 @@
 package com.project200.undabang.common.notification.notifier.impl;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
+@Testcontainers
+@SpringBootTest
 class SlackNotifierAdapterTest {
 
-    @Test
-    void testNotify() {
+    @Autowired
+    private SlackNotifierAdapter slackNotifierAdapter;
+
+    // @Container: Testcontainers가 이 컨테이너의 생명주기(시작, 종료)를 관리하게 합니다.
+    @Container
+    static GenericContainer<?> wiremockContainer = new GenericContainer<>(DockerImageName.parse("wiremock/wiremock:3.5.2-1"))
+            .withExposedPorts(8080) // 컨테이너의 8080 포트를 외부에서 사용할 수 있도록 함
+            .waitingFor(Wait.forHttp("/__admin/mappings").forStatusCode(200)) // WireMock이 완전히 실행될 때 까지 기다림
+            .withCommand("--verbose") // WireMock Container가 --verbose 옵션으로 실행되서 wiremock이 받은 요청과 응답에 대한 상세 로그를 출력한다
+            // 테스트 실행 시 콘솔에서 WireMock 로그를 확인할 수 있음
+            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(SlackNotifierAdapterTest.class)));
+
+
+    // @DynamicPropertySource: Spring 애플리케이션의 프로퍼티를 동적으로 설정합니다.
+    // 컨테이너가 뜬 후에야 알 수 있는 동적 포트 정보를 애플리케이션에 알려주기 위해 사용됩니다.
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        String webhookPath = "/mock/slack-webhook";
+        registry.add("notification.slack.webhook-url",
+                () -> String.format("http://%s:%d%s", wiremockContainer.getHost(), wiremockContainer.getFirstMappedPort(), webhookPath));
+
+        // wiremockContainer.getHost() -> "localhost"
+        // wiremockContainer.getFirstMappedPort() -> 매번 바뀌는 랜덤 포트
+    }
+
+    @Nested
+    @DisplayName("notify 메서드 테스트")
+    class NotifyMethodTest {
+        @BeforeEach
+        void setUp() {
+            // WireMock의 정적 클라이언트(stubFor, verify 등)가 어떤 서버를 대상으로 할지 설정합니다.
+            // 이 설정을 통해 Testcontainers가 띄운 동적 주소의 WireMock 서버를 제어할 수 있게 됩니다.
+            WireMock.configureFor(wiremockContainer.getHost(), wiremockContainer.getFirstMappedPort());
+            WireMock.reset();
+        }
+
+        @Test
+        @DisplayName("Slack API 가 200을 반환하면 정상처리")
+        void slackNotifierAdapterSuccessCondition(){
+            // given
+            String message = "테스트 메시지 _ 테스트 코드에서 성공적으로 통과함";
+            String webhookPath = "/mock/slack-webhook";
+
+            // mock stub 설정
+            stubFor(post(urlEqualTo(webhookPath))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody("ok")));
+
+
+            // when
+            slackNotifierAdapter.notify(message);
+
+            // then
+            verify(1, postRequestedFor(urlEqualTo(webhookPath))
+                    .withRequestBody(containing(message)));
+
+        }
+
+        @Test
+        @DisplayName("Slack API 가 500 에러를 반환하면 경고 로그를 남김")
+        void slackNotifierAdapterFailureCondition(){
+            // given
+            String message = "테스트 메시지 _ 테스트 코드에서 실패함";
+            String webhookPath = "/mock/slack-webhook";
+
+            stubFor(post(urlEqualTo(webhookPath))
+                    .willReturn(aResponse()
+                            .withStatus(500)
+                            .withHeader("Content-Type", "application/json")
+                            .withBody("error")));
+
+            // when
+            slackNotifierAdapter.notify(message);
+
+            // then
+            verify(1, postRequestedFor(urlEqualTo(webhookPath))
+                    .withRequestBody(containing(message)));
+        }
     }
 }
