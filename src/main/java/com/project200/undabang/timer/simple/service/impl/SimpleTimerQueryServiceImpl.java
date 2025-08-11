33df4b42029c3e52b92c99464705efbd474754ae
JooH <nexon9093@gmail.com>
2025-08-11 15:42:34 +0900

    feat(심플타이머): 심플타이머 조회기능 개발 (#216)

---
+++ src/main/java/com/project200/undabang/timer/simple/service/impl/SimpleTimerQueryServiceImpl.java
@@ -0,0 +1,48 @@
+package com.project200.undabang.timer.simple.service.impl;
+
+import com.project200.undabang.common.context.UserContextHolder;
+import com.project200.undabang.common.web.exception.CustomException;
+import com.project200.undabang.common.web.exception.ErrorCode;
+import com.project200.undabang.member.entity.Member;
+import com.project200.undabang.member.repository.MemberRepository;
+import com.project200.undabang.timer.simple.dto.GetSimpleTimerResponseDto;
+import com.project200.undabang.timer.simple.dto.SimpleTimerRecord;
+import com.project200.undabang.timer.simple.entity.SimpleTimer;
+import com.project200.undabang.timer.simple.repository.SimpleTimerRepository;
+import com.project200.undabang.timer.simple.service.SimpleTimerQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SimpleTimerQueryServiceImpl implements SimpleTimerQueryService {
+    private final SimpleTimerRepository simpleTimerRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public GetSimpleTimerResponseDto getSimpleTimers(){
+        UUID memberId = UserContextHolder.getUserId();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        List<SimpleTimer> simpleTimerList = simpleTimerRepository.findByMember(member);
+
+        return createSimpleTimerResponse(simpleTimerList);
+    }
+
+    private GetSimpleTimerResponseDto createSimpleTimerResponse(List<SimpleTimer> simpleTimerList){
+        List<SimpleTimerRecord> simpleTimerRecordList = simpleTimerList.stream()
+                .map(SimpleTimerRecord::from)
+                .collect(Collectors.toList());
+
+        return GetSimpleTimerResponseDto.of(simpleTimerRecordList);
+    }
+}
