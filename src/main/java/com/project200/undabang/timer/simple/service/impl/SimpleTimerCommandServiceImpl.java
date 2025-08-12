db8426d3dd97431349fe9aa0094e7491fa9e918a
JooH <nexon9093@gmail.com>
2025-08-13 00:50:19 +0900

    feat(심플타이머) : 회원 인스턴스를 파라미터로 전달받는 방식으로 코드 개선 (#215)

--- src/main/java/com/project200/undabang/timer/simple/service/impl/SimpleTimerCommandServiceImpl.java
+++ src/main/java/com/project200/undabang/timer/simple/service/impl/SimpleTimerCommandServiceImpl.java
@@ -1,10 +1,8 @@
 package com.project200.undabang.timer.simple.service.impl;
 
-import com.project200.undabang.common.context.UserContextHolder;
 import com.project200.undabang.common.web.exception.CustomException;
 import com.project200.undabang.common.web.exception.ErrorCode;
 import com.project200.undabang.member.entity.Member;
-import com.project200.undabang.member.repository.MemberRepository;
 import com.project200.undabang.policy.entity.PolicyKey;
 import com.project200.undabang.policy.service.PolicyService;
 import com.project200.undabang.timer.simple.entity.SimpleTimer;
@@ -15,8 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Service
@@ -25,7 +23,6 @@ import java.util.UUID;
 public class SimpleTimerCommandServiceImpl implements SimpleTimerCommandService {
     private final PolicyService policyService;
     private final SimpleTimerRepository simpleTimerRepository;
-    private final MemberRepository memberRepository;
 
     /**
      * 기본 심플 타이머를 생성하는 메서드입니다.
@@ -41,38 +38,53 @@ public class SimpleTimerCommandServiceImpl implements SimpleTimerCommandService 
      * 오류 발생 시 {@link CustomException} 예외를 발생시킵니다.
      */
     @Override
-    public void createDefaultSimpleTimer() {
+    public void createDefaultSimpleTimer(Member member) {
         int simpleTimerCount = policyService.getPolicyValueAsInt(PolicyKey.SIMPLE_TIMER_INIT_COUNT);
         String simpleTimerValue = policyService.getPolicyValueAsString(PolicyKey.SIMPLE_TIMER_INIT_VALUES);
 
-        List<Integer> timeList = List.of(simpleTimerValue.split(",")).stream()
-                .map(Integer::parseInt)
-                .toList();
+        List<Integer> timeList = getTimeList(simpleTimerValue);
 
         if (!verifyTimerSettings(simpleTimerCount, timeList)) {
-            log.error("정책에 설정된 타이머 갯수와 실제 값의 갯수가 다릅니다.");
+            log.error("정책에 설정된 타이머 개수와 실제 값의 개수가 다릅니다.");
             throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        UUID memberId = UserContextHolder.getUserId();
-        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-
         List<SimpleTimer> simpleTimerList = createSimpleTimerList(member, timeList);
-
         simpleTimerRepository.saveAll(simpleTimerList);
     }
 
     /**
-     * 주어진 회원과 시간 목록을 기반으로 SimpleTimer 객체 리스트를 생성합니다.
+     * 주어진 문자열을 구분자 ','로 분리하여 정수 목록으로 변환하여 반환합니다.
      *
-     * @param member   심플 타이머를 생성할 대상 회원
-     * @param timeList 심플 타이머에 설정할 시간 목록
-     * @return 생성된 SimpleTimer 객체 리스트
+     * @param str 쉼표로 구분된 정수 문자열
+     * @return 문자열로부터 변환된 정수 리스트
+     */
+    private List<Integer> getTimeList(String str) {
+        String[] strArr = str.trim().split(",");
+        List<Integer> integerList = new ArrayList<>();
+
+        for (String s : strArr) {
+            integerList.add(Integer.parseInt(s));
+        }
+
+        return integerList;
+    }
+
+    /**
+     * 회원과 시간 리스트를 기반으로 심플 타이머 객체 리스트를 생성합니다.
+     *
+     * @param member 심플 타이머와 연관된 회원 객체
+     * @param timeList 생성할 심플 타이머의 시간 값 목록
+     * @return 생성된 심플 타이머 객체 리스트
      */
     private List<SimpleTimer> createSimpleTimerList(Member member, List<Integer> timeList) {
-        return timeList.stream()
-                .map(time -> SimpleTimer.of(member, time))
-                .toList();
+        List<SimpleTimer> simpleTimerList = new ArrayList<>();
+
+        for (Integer time : timeList) {
+            simpleTimerList.add(SimpleTimer.of(member, time));
+        }
+
+        return simpleTimerList;
     }
 
     /**
@@ -85,5 +97,4 @@ public class SimpleTimerCommandServiceImpl implements SimpleTimerCommandService 
     private boolean verifyTimerSettings(int simpleTimerCount, List<Integer> simpleTimerList) {
         return simpleTimerCount == simpleTimerList.size();
     }
-
 }
