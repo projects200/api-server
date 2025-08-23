7c82aeac43f10fa13a3703d12632e0253724599d
JooH <nexon9093@gmail.com>
2025-08-23 15:12:15 +0900

    feat(커스텀타이머) : 커스텀 타이머 이름 수정 기능 기본 클래스 생성 및 버전 업데이 (#251)

--- src/main/java/com/project200/undabang/timer/custom/controller/CustomTimerCommandController.java
+++ src/main/java/com/project200/undabang/timer/custom/controller/CustomTimerCommandController.java
@@ -2,14 +2,15 @@ package com.project200.undabang.timer.custom.controller;
 
 import com.project200.undabang.common.web.response.CommonResponse;
 import com.project200.undabang.timer.custom.dto.request.CustomTimerCreateRequest;
-import com.project200.undabang.timer.custom.dto.request.CustomTimerNameUpdateRequest;
 import com.project200.undabang.timer.custom.dto.response.CustomTimerCreateResponse;
 import com.project200.undabang.timer.custom.service.CustomTimerCommandService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,24 +19,9 @@ public class CustomTimerCommandController {
     private final CustomTimerCommandService customTimerCommandService;
 
     @PostMapping("/v1/custom-timers")
-    public ResponseEntity<CommonResponse<CustomTimerCreateResponse>> createCustomTimer(@Valid @RequestBody CustomTimerCreateRequest request) {
+    public ResponseEntity<CommonResponse<CustomTimerCreateResponse>> createCustomTimer(@RequestBody CustomTimerCreateRequest request) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.create(customTimerCommandService.createCustomTimer(request)));
-    }
-
-    @DeleteMapping("/v1/custom-timers/{customTimerId}")
-    public ResponseEntity<CommonResponse<Void>> deleteCustomTimer(@PathVariable Long customTimerId) {
-
-        customTimerCommandService.deleteCustomTimer(customTimerId);
-        return ResponseEntity.ok(CommonResponse.delete(null));
-    }
-
-    @PatchMapping("/v1/custom-timers/{customTimerId}")
-    public ResponseEntity<CommonResponse<Void>> updateCustomTimerName(@PathVariable("customTimerId") Long customTimerId,
-                                                                      @Valid @RequestBody CustomTimerNameUpdateRequest request) {
-
-        customTimerCommandService.updateCustomTimerName(customTimerId, request);
-        return ResponseEntity.ok(CommonResponse.update(null));
     }
 
 }
--- src/main/java/com/project200/undabang/timer/custom/repository/CustomTimerStepRepository.java
+++ src/main/java/com/project200/undabang/timer/custom/repository/CustomTimerStepRepository.java
@@ -6,6 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CustomTimerStepRepository extends JpaRepository<CustomTimerStep, Long>, CustomTimerStepRepositoryCustom {
+public interface CustomTimerStepRepository extends JpaRepository<CustomTimerStep, Long> {
     List<CustomTimerStep> findAllByCustomTimerAndCustomTimerStepDeletedAtNull(CustomTimer customTimer);
+=======
+        import com.project200.undabang.timer.custom.entity.CustomTimerStep;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+    interface CustomTimerStepRepository extends JpaRepository<CustomTimerStep, Long> {
+>>>>>>>16715
+
+                커스텀타이머 생성
+b1(feat(커스텀타이 :
+        기능 기본머)
+
+        클래스 생성(#229))
 }
