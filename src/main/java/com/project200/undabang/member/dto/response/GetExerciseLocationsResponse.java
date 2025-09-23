3d91d8da14d93b8a3aceadaf45054bf7d776b348
JooH <nexon9093@gmail.com>
2025-09-23 17:44:28 +0900

    feat(운동장소) : 운동 장소 목록 조회 기능 개발 (#312)

---
+++ src/main/java/com/project200/undabang/member/dto/response/GetExerciseLocationsResponse.java
@@ -0,0 +1,31 @@
+package com.project200.undabang.member.dto.response;
+
+import com.project200.undabang.member.entity.ExerciseLocation;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetExerciseLocationsResponse {
+    private Long id;
+    private String name;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+
+    public static GetExerciseLocationsResponse from(ExerciseLocation exerciseLocation) {
+        return GetExerciseLocationsResponse.builder()
+                .id(exerciseLocation.getExerciseLocationId())
+                .name(exerciseLocation.getExerciseLocationName())
+                .address(exerciseLocation.getExerciseLocationAddress())
+                // Point 의 Y 좌표는 위도와 매핑됨
+                .latitude(exerciseLocation.getExerciseLocationPoint().getY())
+                // Point 의 X 좌표는 경도와 매핑됨
+                .longitude(exerciseLocation.getExerciseLocationPoint().getX())
+                .build();
+    }
+}
--- src/main/java/com/project200/undabang/member/service/impl/ExerciseLocationQueryServiceImpl.java
+++ src/main/java/com/project200/undabang/member/service/impl/ExerciseLocationQueryServiceImpl.java
@@ -1,8 +1,15 @@
 package com.project200.undabang.member.service.impl;
 
+import com.project200.undabang.common.context.UserContextHolder;
+import com.project200.undabang.common.web.exception.CustomException;
+import com.project200.undabang.common.web.exception.ErrorCode;
 import com.project200.undabang.member.dto.record.MemberProfileAndLocationRecord;
+import com.project200.undabang.member.dto.response.GetExerciseLocationsResponse;
 import com.project200.undabang.member.dto.response.GetMembersExerciseLocationsResponse;
+import com.project200.undabang.member.entity.ExerciseLocation;
+import com.project200.undabang.member.entity.Member;
 import com.project200.undabang.member.repository.ExerciseLocationRepository;
+import com.project200.undabang.member.repository.MemberRepository;
 import com.project200.undabang.member.service.ExerciseLocationQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +24,7 @@ import java.util.*;
 @Transactional(readOnly = true)
 public class ExerciseLocationQueryServiceImpl implements ExerciseLocationQueryService {
     private final ExerciseLocationRepository exerciseLocationRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 모든 회원의 운동 위치 정보를 가져와 반환합니다.
@@ -38,6 +46,24 @@ public class ExerciseLocationQueryServiceImpl implements ExerciseLocationQuerySe
     }
 
     /**
+     * 현재 사용자와 관련된 운동 위치 정보를 가져와 반환합니다.
+     * 이 정보는 삭제되지 않은 운동 위치를 대상으로 하며, 각 위치는
+     * GetExerciseLocationsResponse 객체로 매핑됩니다.
+     *
+     * @return 현재 사용자와 관련된 운동 위치 정보를 포함하는 GetExerciseLocationsResponse 객체의 리스트
+     */
+    @Override
+    public List<GetExerciseLocationsResponse> getExerciseLocations() {
+        Member member = getMember(UserContextHolder.getUserId());
+
+        List<ExerciseLocation> exerciseLocationList = exerciseLocationRepository.findByMemberAndExerciseLocationDeletedAtNull(member);
+
+        return exerciseLocationList.stream()
+                .map(GetExerciseLocationsResponse::from)
+                .toList();
+    }
+
+    /**
      * 회원의 프로필 및 위치 정보 리스트를 회원 ID별로 그룹핑하여 Map으로 반환합니다.
      *
      * @param memberProfileAndLocationList 그룹핑되지 않은 전체 DTO 리스트
@@ -53,5 +79,17 @@ public class ExerciseLocationQueryServiceImpl implements ExerciseLocationQuerySe
         }
 
         return groupedMemberLocationsMap;
+    }
+
+    /**
+     * 주어진 회원 ID에 해당하는 회원 정보를 가져옵니다.
+     * 회원 정보가 존재하지 않을 경우 예외를 발생시킵니다.
+     *
+     * @param memberId 회원의 고유 식별자(UUID)
+     * @return 주어진 ID에 해당하는 회원 엔티티
+     * @throws CustomException ErrorCode.MEMBER_NOT_FOUND가 포함된 예외가 발생할 수 있습니다.
+     */
+    private Member getMember(UUID memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
     }
 }
