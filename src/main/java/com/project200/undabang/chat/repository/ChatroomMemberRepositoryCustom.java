bc283686e9b1dfed1db07de461f2a57143c28346
JooH <nexon9093@gmail.com>
2025-10-27 16:51:54 +0900

    feat(메시지): 차단된 회원에게 메시지 전송 불가 처리 및 관련 테스트 및 명세서 추가 (#375)

--- src/main/java/com/project200/undabang/chat/repository/ChatroomMemberRepositoryCustom.java
+++ src/main/java/com/project200/undabang/chat/repository/ChatroomMemberRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.project200.undabang.chat.repository;
 
 import com.project200.undabang.chat.dto.response.GetMemberChatroomResponse;
+import com.project200.undabang.chat.entity.Chatroom;
 import com.project200.undabang.chat.entity.ChatroomMemberStatus;
 import com.project200.undabang.member.entity.Member;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface ChatroomMemberRepositoryCustom {
     List<GetMemberChatroomResponse> getChatroomListByMemberId(Member member);
     Optional<ChatroomMemberStatus> getOpponentStatusByChatroomId(Long chatroomId, Member member);
+
+    boolean checkOtherMemberBlocked(Chatroom currentChatroom, Member currentMember);
 }
