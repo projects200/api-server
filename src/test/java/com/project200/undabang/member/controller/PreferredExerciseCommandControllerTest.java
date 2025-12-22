87e562d65443df638771d5af900693442138e1cd
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2025-12-22 16:26:16 +0900

    [FEAT] 선호 운동 DELETE API 테스트 코드

--- src/test/java/com/project200/undabang/member/controller/PreferredExerciseCommandControllerTest.java
+++ src/test/java/com/project200/undabang/member/controller/PreferredExerciseCommandControllerTest.java
@@ -163,7 +163,7 @@ class PreferredExerciseCommandControllerTest extends AbstractRestDocSupport {
                                         .content(objectMapper.writeValueAsString(request)))
                                         .andExpect(status().isOk())
                                         .andDo(org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document(
-                                                        "delete-my-preferred-exercises/delete-my-preferred-exercises_-success",
+                                                        "delete-my-preferred-exercises",
                                                         requestHeaders(
                                                                         RestDocsUtils.HEADER_ACCESS_TOKEN),
                                                         requestFields(
