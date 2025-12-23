3c8c30442e805dea42c436bbc4575bea61deb570
CHONGMYOUNG LEE <53869165+dlchdaud123@users.noreply.github.com>
2025-12-24 03:44:17 +0900

    [ADD] @NotEmpty -> @NotNull + @Size 으로 변경

--- src/main/java/com/project200/undabang/member/dto/request/DeletePreferredExerciseRequest.java
+++ src/main/java/com/project200/undabang/member/dto/request/DeletePreferredExerciseRequest.java
@@ -1,6 +1,7 @@
 package com.project200.undabang.member.dto.request;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class DeletePreferredExerciseRequest {
-    @NotEmpty(message = "삭제할 운동 ID 목록은 필수입니다.")
+    @NotNull(message = "삭제할 운동 ID 목록은 필수입니다.")
+    @Size(min = 1, max = 5)
     private List<Long> preferredExerciseIds;
 }
