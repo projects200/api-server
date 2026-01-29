2bd607f33704f88f457546acb1f7fa3939ac7ccb
JooH <nexon9093@gmail.com>
2026-01-29 19:09:09 +0900

    feat(피드): PR 리뷰 반영

--- src/main/java/com/project200/undabang/feed/controller/FeedCommandController.java
+++ src/main/java/com/project200/undabang/feed/controller/FeedCommandController.java
@@ -4,6 +4,7 @@ import com.project200.undabang.common.web.response.CommonResponse;
 import com.project200.undabang.feed.dto.request.CreateFeedRequest;
 import com.project200.undabang.feed.dto.response.CreateFeedResponse;
 import com.project200.undabang.feed.service.FeedCommandService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +20,11 @@ public class FeedCommandController {
 
     private final FeedCommandService feedCommandService;
 
+    /**
+     * 사용자가 제공한 요청 데이터를 기반으로 새로운 피드 리소스를 생성합니다.
+     */
     @PostMapping("/v1/feeds")
-    public ResponseEntity<CommonResponse<CreateFeedResponse>> createMemberFeed(@RequestBody CreateFeedRequest request) {
+    public ResponseEntity<CommonResponse<CreateFeedResponse>> createMemberFeed(@Valid @RequestBody CreateFeedRequest request) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.create(feedCommandService.createMemberFeed(request)));
     }
