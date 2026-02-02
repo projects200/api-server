d932c36935673d30e99c7911f6c537fa246fa7d8
JooH <nexon9093@gmail.com>
2026-02-02 21:01:20 +0900

    feat(피드) : 피드 사진 업로드 및 최대 갯수 제한 로직 구현 [0.11.10] (#504)

--- src/main/java/com/project200/undabang/feed/controller/FeedCommandController.java
+++ src/main/java/com/project200/undabang/feed/controller/FeedCommandController.java
@@ -1,23 +1,35 @@
 package com.project200.undabang.feed.controller;
 
+import com.project200.undabang.common.validation.AllowedExtensions;
 import com.project200.undabang.common.web.response.CommonResponse;
 import com.project200.undabang.feed.dto.request.CreateFeedRequest;
 import com.project200.undabang.feed.dto.request.UpdateFeedRequest;
+import com.project200.undabang.feed.dto.response.CreateFeedPictureResponse;
 import com.project200.undabang.feed.dto.response.CreateFeedResponse;
 import com.project200.undabang.feed.dto.response.UpdateFeedResponse;
 import com.project200.undabang.feed.service.FeedCommandService;
+import com.project200.undabang.feed.service.FeedPictureService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class FeedCommandController {
 
     private final FeedCommandService feedCommandService;
+    private final FeedPictureService feedPictureService;
 
     /**
      * 사용자가 제공한 요청 데이터를 기반으로 새로운 피드 리소스를 생성합니다.
@@ -46,5 +58,15 @@ public class FeedCommandController {
     public ResponseEntity<CommonResponse<Void>> deleteMemberFeed(@PathVariable Long feedId) {
 
         return ResponseEntity.ok(CommonResponse.delete(feedCommandService.deleteMemberFeed(feedId)));
+    }
+
+    @PostMapping(value = "/v1/feeds/{feedId}/pictures", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CommonResponse<List<CreateFeedPictureResponse>>> createFeedPictures(
+            @PathVariable @Positive(message = "올바른 피드 식별자를 입력해주세요") Long feedId,
+            @Size(max = 5, message = "최대 5개의 파일만 업로드 할 수 있습니다.")
+            @AllowedExtensions(extensions = {".jpg", ".jpeg", ".png"})
+            @RequestParam("pictures") List<MultipartFile> feedPictureList) {
+
+        return ResponseEntity.ok(CommonResponse.create(feedPictureService.createFeedPictures(feedId, feedPictureList)));
     }
 }
---
+++ src/main/java/com/project200/undabang/feed/service/FeedPictureService.java
@@ -0,0 +1,10 @@
+package com.project200.undabang.feed.service;
+
+import com.project200.undabang.feed.dto.response.CreateFeedPictureResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface FeedPictureService {
+    List<CreateFeedPictureResponse> createFeedPictures(Long feedId, List<MultipartFile> imageFileList);
+}
