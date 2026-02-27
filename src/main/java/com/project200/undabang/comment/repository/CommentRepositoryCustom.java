c2474b129369be4fb06aca461d64835d87a48093
dlchdaud123 <dlchdaud321@gmail.com>
2026-02-27 17:30:37 +0900

    [FIX]댓글 좋아요 여부 표시 오류 수정

--- src/main/java/com/project200/undabang/comment/repository/CommentRepositoryCustom.java
+++ src/main/java/com/project200/undabang/comment/repository/CommentRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.project200.undabang.comment.repository;
 
 import com.project200.undabang.comment.dto.response.CommentResponse;
+import com.project200.undabang.member.entity.Member;
 
 import java.util.List;
 
 public interface CommentRepositoryCustom {
 
-    List<CommentResponse> findCommentsWithChildrenByFeedId(Long feedId);
+    List<CommentResponse> findCommentsWithChildrenByFeedId(Long feedId, Member currentMember);
 }
